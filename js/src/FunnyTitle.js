67c68cab7da2f10f39035a9f2a8f91d81de6ae13
morningstar <a1228136188@163.com>
2021-10-09 09:43:15 +0800

    Site updated: 2021-10-09 09:43:14

---
+++ js/src/FunnyTitle.js
@@ -0,0 +1,16 @@
+ var OriginTitle = document.title;
+ var titleTime;
+ document.addEventListener('visibilitychange', function () {
+     if (document.hidden) {
+         $('[rel="icon"]').attr('href', "/img/trhx2.png");
+         document.title = 'ヽ(●-`Д´-)ノ客官别走!!!';
+         clearTimeout(titleTime);
+     }
+     else {
+         $('[rel="icon"]').attr('href', "/img/trhx2.png");
+         document.title = 'ヾ(Ő∀Ő3)ノ客官欢迎回来！' + OriginTitle;
+         titleTime = setTimeout(function () {
+             document.title = OriginTitle;
+         }, 2000);
+     }
+ });
